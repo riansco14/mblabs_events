67eac3f449c56fcbfe4c53ffb9d4c3a5c0a49daf
Rian <rian.azevedo@qintess.com>
2023-08-14 17:01:17 -0300

    fix: -add safeareaview

--- App.tsx
+++ App.tsx
@@ -2,8 +2,9 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 import theme from './src/config/styles/theme';
 import { Routes } from './src/routes';
 
@@ -11,18 +12,11 @@ import { Routes } from './src/routes';
 
 export default function App() {
   return (<ThemeProvider theme={theme}>
+    <SafeAreaView style={{flex: 1}}>
       <Routes/>
       <StatusBar style="auto" />
+      </SafeAreaView>
   </ThemeProvider>
     
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
