dbdcbbdd396f2ca13f18246b9aa9614d0f309722
Rian <rian.azevedo@qintess.com>
2023-08-15 19:17:06 -0300

    create: -component event card

---
+++ src/components/EventCard/index.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+    ButtonsContainer,
+  Container,
+  EventImage,
+  EventInfoContainer,
+  LocationEventContainer,
+} from "./styles";
+import { Text } from "../Text";
+import { useTheme } from "styled-components";
+import { Icon } from "../Icon";
+import { SocialButton } from "../SocialButton";
+
+interface EventCardProps{
+    id: number
+    dateString: string
+    eventName: string
+    localName: string
+}
+
+const temporarySwitchImages = (id: number)=>{
+    switch(id){
+        case 1:
+            return require("../../../database/banda2.png")
+        case 2:
+            return require("../../../database/banda3.png")
+        case 3:
+            return require("../../../database/banda4.png")
+    }
+}
+
+
+export function EventCard({id, dateString, eventName, localName}: EventCardProps) {
+  const theme = useTheme();
+
+  const image = temporarySwitchImages(id)
+  return (
+    <Container>
+      <EventImage source={image}></EventImage>
+      <EventInfoContainer>
+        <Text type="small" color={theme.colors.font_dark}>
+          {dateString}
+        </Text>
+        <Text type="defaultBold" color={theme.colors.font_dark}>
+          {eventName}
+        </Text>
+        <LocationEventContainer>
+          <Icon
+            name="map_pin"
+            color={theme.colors.font_dark}
+            width={14}
+            height={14}
+          />
+          <Text type="small" color={theme.colors.font_dark} style={{marginLeft: 4}}>
+            {localName}
+          </Text>
+        </LocationEventContainer>
+      </EventInfoContainer>
+      <ButtonsContainer>
+        <SocialButton name="heart" />
+        <SocialButton name="share" style={{ marginLeft: 8 }} />
+      </ButtonsContainer>
+    </Container>
+  );
+}
---
+++ src/components/EventCard/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components/native"
+
+export const Container = styled.TouchableOpacity`
+    flex-direction: row;
+    width: 100%;
+    height: 84px;
+
+    padding-right: 12px;
+`
+
+
+export const EventImage = styled.Image`
+    width: 90px;
+    height: 84px;
+`
+
+
+export const EventInfoContainer = styled.View`
+    flex: 1;
+    margin-left: 12px;
+    justify-content: space-evenly;
+`
+
+export const LocationEventContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+
+export const ButtonsContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+
+    margin-top: 4px;
+`
+
+
+
+
