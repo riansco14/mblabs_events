6b4cac9ac15d185e0ff21109f636d1355d3b7fae
Rian <rian.azevedo@qintess.com>
2023-08-14 21:23:33 -0300

    create: -add font semibold -add colors and fonts info in theme

--- App.tsx
+++ App.tsx
@@ -19,6 +19,7 @@ export default function App() {
   const [fontsLoaded, fontError] = useFonts({
     'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
     'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
     'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
   });
 
--- src/config/styles/theme.ts
+++ src/config/styles/theme.ts
@@ -1,11 +1,31 @@
 export default {
     colors: {
-        primary: '',
-        secondary: '',
+        primary: '#0DCDAA',
+        white: '#FFF',
+        black: "#000",
+        grey: "#F2F2F2",
+        grey_light: "#FBFBFB",
+
+        font_light: "#FFF",
+        font_dark: "#262627",
+        font_grey: "#7C7C7C",
     },
     fonts: {
         regular: 'Inter-Regular',
         medium : 'Inter-Medium',
+        semibold: "Inter-SemiBold",
         bold : 'Inter-Bold',
     },
+    fontSizes: {
+        h1: "40px",
+        h2: "32px",
+        h3: "28px",
+        h4: "24px",
+        h5: "20px",
+        h6: "18px",
+
+        body: "16px",
+        body_small: "12px"
+    }
+
 };
