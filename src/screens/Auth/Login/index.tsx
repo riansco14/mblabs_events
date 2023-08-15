001a3cc225d832f35d7d4f3659d6619336983f7a
Rian <rian.azevedo@qintess.com>
2023-08-15 12:27:02 -0300

    create: -screen login

--- src/screens/Auth/Login/index.tsx
+++ src/screens/Auth/Login/index.tsx
@@ -1,9 +1,91 @@
-import React from "react"
-import { Container } from "./styles"
-import { Text } from "react-native"
+import React, { useState, useRef } from "react";
+import {
+  Container,
+  Footer,
+  FormContainer,
+  LogoContainer,
+  RememberPasswordContainer,
+  WrapperContainer,
+} from "./styles";
+import { Text } from "../../../components/Text";
+import { useTheme } from "styled-components";
+import { EditText } from "../../../components/EditText";
+import { Heading } from "../../../components/Heading";
+import { Button } from "../../../components/Button";
+import { Switch, TextInput } from "react-native-gesture-handler";
 
-export function Login (){
-    return(<Container>
-        <Text>Login</Text>
-    </Container>)
+import Logo from "../../../assets/ilustrations/logoEventsColor.svg";
+import { StatusBar } from "expo-status-bar";
+
+export function Login() {
+  const theme = useTheme();
+  const [rememberPassword, setRememberPassword] = useState(true);
+
+  const refPasswordInput = useRef<TextInput | null>();
+  function handleLogin() {}
+
+  return (
+    <Container>
+      <StatusBar
+        backgroundColor={theme.colors.primary}
+        style="light"
+      ></StatusBar>
+      <WrapperContainer>
+        <FormContainer>
+          <LogoContainer>
+            <Logo width={160} height={160} />
+            <Heading type="h4">Bem vindo, novamente!</Heading>
+          </LogoContainer>
+
+          <Text
+            type="defaultBold"
+            color={theme.colors.font_grey}
+            style={{ marginTop: 60 }}
+          >
+            E-mail
+          </Text>
+          <EditText
+            placeholder="name@example.com"
+            returnKeyType="next"
+            onSubmitEditing={() => refPasswordInput.current?.focus()}
+            blurOnSubmit={false}
+          />
+          <Text
+            type="defaultBold"
+            color={theme.colors.font_grey}
+            style={{ marginTop: 30 }}
+          >
+            Senha
+          </Text>
+          <EditText ref={refPasswordInput} placeholder="Enter your password" />
+          <RememberPasswordContainer>
+            <Text type="default" color={theme.colors.font_grey}>
+              Lembrar senha
+            </Text>
+            <Switch
+              trackColor={{
+                false: theme.colors.font_grey,
+                true: theme.colors.primary,
+              }}
+              thumbColor={theme.colors.white}
+              onValueChange={() => {
+                setRememberPassword((oldState) => !oldState);
+              }}
+              value={rememberPassword}
+            />
+          </RememberPasswordContainer>
+        </FormContainer>
+        <Footer>
+          <Button
+            type="default"
+            backgroundColor={theme.colors.primary}
+            textColor={theme.colors.white}
+            onPress={handleLogin}
+          >
+            Logar
+          </Button>
+        </Footer>
+      </WrapperContainer>
+    </Container>
+  );
 }
--- src/screens/Auth/Login/styles.ts
+++ src/screens/Auth/Login/styles.ts
@@ -1,5 +1,36 @@
 import styled from "styled-components/native"
+import { Dimensions } from "react-native"
 
 export const Container = styled.View`
-    flex: 1;
+    height: 100%;
+    width: 100%;
+    background-color: ${({theme})=>theme.colors.white};
 `
+
+
+export const WrapperContainer = styled.View`
+    padding: 0 40px;
+    justify-content: space-between;
+`
+
+export const FormContainer = styled.View`
+`
+
+
+export const LogoContainer = styled.View`
+    width: 100%;
+    align-items: center;
+`
+
+export const RememberPasswordContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-top: 30px;
+`
+
+export const Footer = styled.View`
+    margin-top: 30px;
+`
+
