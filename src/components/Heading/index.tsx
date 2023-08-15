73fe207d9d71897f03c408e4bd5801fe48a3bd07
Rian <rian.azevedo@qintess.com>
2023-08-14 22:10:14 -0300

    fix: -add interface text - fix sizes

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -2,15 +2,14 @@ import React from "react"
 import { Container } from "./styles"
 
 import { useTheme } from 'styled-components'
-import {StyleProp,ViewStyle } from "react-native"
+import {TextProps } from "react-native"
 
-interface HeadingProps{
+interface HeadingProps extends TextProps{
     children: React.ReactNode
     type: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
-    style?: StyleProp<ViewStyle>
 }
 
-export function Heading ({children, type = "h6", style}: HeadingProps){
+export function Heading ({children, type = "h6", ...rest}: HeadingProps){
     const theme = useTheme()
     const headingStyles = {
         h1 : {
@@ -18,23 +17,23 @@ export function Heading ({children, type = "h6", style}: HeadingProps){
             fontFamily: theme.fonts.bold,
         },
         h2 : {
-            fontSize: theme.fontSizes.h1,
+            fontSize: theme.fontSizes.h2,
             fontFamily: theme.fonts.bold,
         },
         h3 : {
-            fontSize: theme.fontSizes.h1,
+            fontSize: theme.fontSizes.h3,
             fontFamily: theme.fonts.semibold,
         },
         h4 : {
-            fontSize: theme.fontSizes.h1,
+            fontSize: theme.fontSizes.h4,
             fontFamily: theme.fonts.semibold,
         },
         h5 : {
-            fontSize: theme.fontSizes.h1,
+            fontSize: theme.fontSizes.h5,
             fontFamily: theme.fonts.bold,
         },
         h6 : {
-            fontSize: theme.fontSizes.h1,
+            fontSize: theme.fontSizes.h6,
             fontFamily: theme.fonts.semibold,
         },
     }
@@ -42,7 +41,7 @@ export function Heading ({children, type = "h6", style}: HeadingProps){
     return(<Container 
         fontSize={headingStyles[type].fontSize} 
         fontFamily={headingStyles[type].fontFamily} 
-        style={style}
+        {...rest}
         >
         {children}
     </Container>)
