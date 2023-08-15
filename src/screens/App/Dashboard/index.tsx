879ed9460ea3c451fde37a83623ab63c96d8f2c2
Rian <rian.azevedo@qintess.com>
2023-08-15 19:19:15 -0300

    remove: -old files

--- src/screens/App/Dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { Container } from "./styles"
-import { Text } from "react-native"
-
-export function Dashboard (){
-    return(<Container>
-        <Text style={{fontFamily: "Inter-SemiBold", fontSize: 22}}>Dashboard</Text>
-    </Container>)
-}
