66ef31780fbc02b7a081a4890c30c84e715aec8d
Rian <rian.azevedo@qintess.com>
2023-08-15 16:32:28 -0300

    change: -add login in redux -type for user

--- src/store/user/userSlice.ts
+++ src/store/user/userSlice.ts
@@ -1,12 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../store'
+import { UserType } from '../../common/types'
 
 interface UserState {
   userRemember: {
     email: string
     rememberStatus: boolean
   }
+  userLogged?: UserType
 }
 
 const initialState: UserState = {
@@ -14,13 +16,34 @@ const initialState: UserState = {
     email: "",
     rememberStatus: false
   },
+  userLogged: null,
+}
 
+interface UserLoginType{
+  email: string
+  password: string
 }
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    login: (state, action: PayloadAction<UserLoginType>) => {
+      const { email, password } = action.payload
+
+      if(email === "admin@gmail.com" && password === "admin"){
+        const userFakeLogged = {
+          id: 1234,
+          email: "admin@gmail.com",
+          username: "Rian R"
+        }
+
+        state.userLogged = userFakeLogged
+      }
+    },
+    logout: (state) => {
+      state.userLogged = null
+    },
     setUserEmailRemember: (state, action: PayloadAction<string>) => {
       state.userRemember.email = action.payload
       state.userRemember.rememberStatus = action.payload ? true: false
@@ -28,7 +51,7 @@ export const userSlice = createSlice({
   },
 })
 
-export const { setUserEmailRemember } = userSlice.actions
+export const { login, logout, setUserEmailRemember } = userSlice.actions
 
 export const selectCount = (state: RootState) => state.user
 
