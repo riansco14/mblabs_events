c766b9b5e2681cbd809a0ddca854972dbf020449
Rian <rian.azevedo@qintess.com>
2023-08-19 23:40:36 -0300

    fix: -add a param in route type

--- src/config/navigation/routes.ts
+++ src/config/navigation/routes.ts
@@ -1,16 +1,18 @@
-import type { CompositeNavigationProp } from '@react-navigation/native';
-import type { StackNavigationProp } from '@react-navigation/stack';
-import type { BottomTabNavigationProp  } from '@react-navigation/bottom-tabs';
+import type { CompositeNavigationProp } from "@react-navigation/native";
+import type { StackNavigationProp } from "@react-navigation/stack";
+import type { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 
 type AuthStackParamList = {
-    Welcome: undefined;
-    Login: undefined;
-}
-
+  Welcome: undefined;
+  Login: undefined;
+};
 
 type AppStackParamList = {
-    BottomTabs: undefined;
-    TicketInfo: undefined;
-}
+  BottomTabs: undefined;
+  TicketInfo: { idEvent: number };
+};
 
-export type AuthStackParam = CompositeNavigationProp<StackNavigationProp<AuthStackParamList>, BottomTabNavigationProp<AppStackParamList>>
+export type AuthStackParam = CompositeNavigationProp<
+  StackNavigationProp<AuthStackParamList>,
+  BottomTabNavigationProp<AppStackParamList>
+>;
