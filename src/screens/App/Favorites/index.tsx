e13cdec4c92596db9b2651fc3ee308bcd094de09
Rian <rian.azevedo@qintess.com>
2023-08-20 01:35:25 -0300

    change: -add event favorites counter -add event to share and like

--- src/screens/App/Favorites/index.tsx
+++ src/screens/App/Favorites/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, HeadingContainer } from "./styles";
 import { Heading } from "../../../components/Heading";
 import { FlatList, View } from "react-native";
 import { EventCard } from "../../../components/EventCard";
@@ -7,6 +7,9 @@ import { EventCardType } from "../../../common/types";
 import { useAppDispatch, useAppSelector } from "../../../store/hook";
 import { addOrRemoveEventFavorite } from "../../../store/event/eventSlice";
 import { getEvents } from "../../../../database/db";
+import theme from "../../../config/styles/theme";
+import { Text } from "../../../components/Text";
+import { shareEvent } from "../../../services/system/System";
 
 export function Favorites() {
   const dispatch = useAppDispatch();
@@ -14,7 +17,7 @@ export function Favorites() {
     (state) => state.event.eventsFavorites
   );
 
-  const events: EventCardType[] = getEvents()
+  const events: EventCardType[] = getEvents();
 
   const eventsCommon = events.filter(
     (item) => !!eventsFavorites.find((itemStore) => itemStore === item.idEvent)
@@ -26,17 +29,39 @@ export function Favorites() {
 
   return (
     <Container>
-      <Heading type="h5">Favoritos</Heading>
+      <HeadingContainer>
+        <Heading type="h5">Favoritos</Heading>
+        {eventsFavorites.length > 0 ? (
+          <Text
+            type="defaultBold"
+            style={{
+              textAlign: "center",
+              backgroundColor: theme.colors.primary,
+              width: 30,
+              height: 30,
+              borderRadius: 30,
+              paddingTop: 3,
+              marginLeft: 16,
+            }}
+            color={theme.colors.white}
+          >
+            {eventsFavorites.length}
+          </Text>
+        ) : null}
+      </HeadingContainer>
 
       <FlatList
         data={eventsCommon}
         renderItem={({ item }) => (
           <EventCard
-            {...item}
-            isLiked={
-              eventsFavorites.includes(item.idEvent)
-            }
-            onPressLikeButton={() => handleAddOrRemoveFavorite(item.idEvent)}
+            eventData={item}
+            isLiked={eventsFavorites.includes(item.idEvent)}
+            onPressLikeButton={() => {
+              handleAddOrRemoveFavorite(item.idEvent);
+            }}
+            onPressShareButton={()=>{
+              shareEvent(item)
+            }}
           />
         )}
         style={{ marginTop: 20 }}
--- src/screens/App/Favorites/styles.ts
+++ src/screens/App/Favorites/styles.ts
@@ -3,4 +3,10 @@ import styled from "styled-components/native"
 export const Container = styled.View`
     flex: 1;
     padding: 16px;
+    background-color: ${({theme})=>theme.colors.white};
+`
+
+
+export const HeadingContainer = styled.View`
+    flex-direction: row;
 `
