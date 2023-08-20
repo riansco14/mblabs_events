a68ec23a4db7dfd05d6fe64a155a09b1f0aee30d
Rian <rian.azevedo@qintess.com>
2023-08-20 01:32:12 -0300

    create: -add event in calendar -share event info

--- src/services/system/System.ts
+++ src/services/system/System.ts
@@ -1,5 +1,7 @@
-import { Linking, Platform } from "react-native";
-
+import * as AddCalendarEvent from "react-native-add-calendar-event";
+import { Linking, Platform, Share } from "react-native";
+import { EventCardType } from "../../common/types";
+import moment from "moment"
 interface CoordsInterface {
   latitude: string;
   longitude: string;
@@ -13,3 +15,51 @@ export function openMaps({ latitude, longitude, label }: CoordsInterface) {
   });
   Linking.openURL(url);
 }
+
+interface EventCalendarInterface {
+  title: string;
+  startDate?: string;
+  endDate?: string;
+}
+
+export function createEventInCalendar(eventConfig: EventCalendarInterface) {
+  console.log(eventConfig);
+  
+  
+  AddCalendarEvent.presentEventCreatingDialog(eventConfig)
+    .then(
+      (eventInfo: {
+        calendarItemIdentifier: string;
+        eventIdentifier: string;
+      }): void => {
+        // handle success - receives an object with `calendarItemIdentifier` and `eventIdentifier` keys, both of type string.
+        // These are two different identifiers on iOS.
+        // On Android, where they are both equal and represent the event id, also strings.
+        // when { action: 'CANCELED' } is returned, the dialog was dismissed
+        console.warn(JSON.stringify(eventInfo));
+      }
+    )
+    .catch((error: string) => {
+      // handle error such as when user rejected permissions
+      console.warn(error);
+    });
+}
+
+export function shareEvent(eventData: EventCardType) {
+  const shareOptions = {
+      title: "Compartilhar Evento",
+      message: `Você sabia que vai ter o evento ${eventData.eventName} no ${
+        eventData.locationName
+      } no dia ${moment(eventData.dateInfo.startDate).format("D/MM à\\s kk:mm")}`,
+      subject: "Subject",
+  };
+  
+  Share.share(shareOptions);
+}
+
+
+
+
+
+
+
