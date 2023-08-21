df54a5e2b6b9740d53436b89a7ea28512a2eafe3
Rian <rian.azevedo@qintess.com>
2023-08-20 22:44:52 -0300

    change: -add button register and navigate

--- src/screens/Auth/Welcome/index.tsx
+++ src/screens/Auth/Welcome/index.tsx
@@ -1,52 +1,65 @@
-import React from "react"
-import { Container } from "./styles"
-import { Heading } from "../../../components/Heading"
+import React from "react";
+import { Container } from "./styles";
+import { Heading } from "../../../components/Heading";
 
-import WelcomePeople from "./../../../assets/ilustrations/welcomePeople.svg"
-import { Text } from "../../../components/Text"
-import { useTheme } from "styled-components"
-import { Button } from "../../../components/Button"
-import {useNavigation} from "@react-navigation/native"
-import { AuthStackParam } from "../../../config/navigation/routes"
+import WelcomePeople from "./../../../assets/ilustrations/welcomePeople.svg";
+import { Text } from "../../../components/Text";
+import { useTheme } from "styled-components";
+import { Button } from "../../../components/Button";
+import { useNavigation } from "@react-navigation/native";
+import { AuthStackParam } from "../../../config/navigation/routes";
 
 import Logo from "../../../assets/ilustrations/logoEventsColor.svg";
+import { ButtonUnderlined } from "../../../components/ButtonUnderlined";
 
-export function Welcome (){
-    const theme = useTheme()
-    const navigation = useNavigation<AuthStackParam>()
+export function Welcome() {
+  const theme = useTheme();
+  const navigation = useNavigation<AuthStackParam>();
 
-    function handleLogin() {
-        navigation.navigate("Login")
-    }
+  function handleLogin() {
+    navigation.navigate("Login");
+  }
 
-    return(<Container>
-            <Logo width={120} height={120} />
-            <Heading 
-                type="h2"
-                style={{marginTop: 0}}
-            >
-                Bem vindo!
-            </Heading>
-            <Text 
-                type="small" 
-                color={theme.colors.font_grey}
-                style={{marginTop: 12}}
-                
-                >Entre na sua conta e procure algum evento</Text>
-            <WelcomePeople 
-                style={{marginTop: 60}}
-            />
+  function handleRegister() {
+    navigation.navigate("Register");
+  }
 
-            <Button 
-                type="default" 
-                backgroundColor={theme.colors.primary} 
-                textColor={theme.colors.white}
+  return (
+    <Container>
+      <Logo width={120} height={120} />
+      <Heading type="h2" style={{ marginTop: 0 }}>
+        Bem vindo!
+      </Heading>
+      <Text
+        type="small"
+        color={theme.colors.font_grey}
+        style={{ marginTop: 12 }}
+      >
+        Entre na sua conta e procure algum evento
+      </Text>
+      <WelcomePeople style={{ marginTop: 60 }} />
 
-                style={{marginTop: 100}}
+      <Button
+        type="default"
+        backgroundColor={theme.colors.primary}
+        textColor={theme.colors.white}
+        style={{ marginTop: 100 }}
+        onPress={handleLogin}
+      >
+        Logar
+      </Button>
 
-                onPress={handleLogin}
-                >
-                    Logar
-            </Button>
-    </Container>)
+      <ButtonUnderlined
+        type="default"
+        textColor={theme.colors.font_grey}
+        style={{ marginTop: 20 }}
+        onPress={handleRegister}
+      >
+        Não tem conta?{" "}
+        <Text type="defaultBold" color={theme.colors.primary}>
+          Registre-se
+        </Text>
+      </ButtonUnderlined>
+    </Container>
+  );
 }
