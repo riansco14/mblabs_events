315089f61da87968335325f7830c38467179c24f
Rian <rian.azevedo@qintess.com>
2023-08-20 22:50:53 -0300

    create: -create more types -add more data in mock

--- database/db.ts
+++ database/db.ts
@@ -1,4 +1,4 @@
-import { EventCardType } from "../src/common/types";
+import { CityType, EventCardType, TicketType } from "../src/common/types";
 
 function login({ email, password }) {
   if (email === "admin@gmail.com" && password === "admin")
@@ -10,87 +10,233 @@ function login({ email, password }) {
   throw Error("Not Found");
 }
 
-export const getEvents = (): EventCardType[] =>  [
+export const getCities = (): CityType[] => [
   {
-    idEvent: 4,
-    eventName: "La Rosalia",
-    locationName: "Maracanã",
-    locationAddress: "Maracanã, Rio de Janeiro",
-    locationGPS: {
-      latitude: "-22.912324276023295",
-      longitude: "-43.226815212389724",
-      label: "Maracanã"
-    },
-    dateInfo:{
-      startDate: 1700514000694,
-      endDate: 1700521200694
-    },
-    highlight: true,
-    price: "R$ 50,00",
+    id: 1,
+    state: "Pernambuco",
+    city: "Recife",
   },
   {
-    idEvent: 1,
-    eventName: "The Kooks",
-    locationName: "Razzmatazz",
-    locationAddress: "Avenida Recife, 386, 56800-000",
-    locationGPS: {
-      latitude: "-8.034432794113949",
-      longitude: "-34.87095323678137",
-      label: "Classic Hall"
-    },
-    dateInfo:{
-      startDate: 1700514000694,
-      endDate: 1700521200694
-    },
-    price: "R$ 10,00",
+    id: 2,
+    state: "São Paulo",
+    city: "São Paulo",
   },
   {
-    idEvent: 2,
-    eventName: "The Wombats",
-    locationName: "Sala Apolo",
-    locationAddress: "Avenida Recife, 386, 56800-000",
-    locationGPS: {
-      latitude: "-8.034432794113949",
-      longitude: "-34.87095323678137",
-      label: "Classic Hall"
-    },
-    dateInfo:{
-      startDate: 1700514000694,
-      endDate: 1700521200694
-    },
-    price: "R$ 20,00",
-  },
-  {
-    idEvent: 3,
-    eventName: "Foster The People",
-    locationName: "La Monumental",
-    locationAddress: "Avenida Recife, 386, 56800-000",
-    locationGPS: {
-      latitude: "-8.034432794113949",
-      longitude: "-34.87095323678137",
-      label: "Classic Hall"
-    },
-    dateInfo:{
-      startDate: 1700514000694,
-      endDate: 1700521200694
-    },
-    price: "R$ 30,00",
+    id: 3,
+    state: "Paraná",
+    city: "Curitiba",
   },
 ];
 
-export const getEvent = (idEvent: number) =>{
-  return getEvents().find(item=>item.idEvent === idEvent)
-}
+export const getEvents = (idCity: number): EventCardType[] => {
+  const eventsDB = [
+    {
+      idEvent: 1,
+      eventName: "The Kooks",
+      locationName: "Razzmatazz",
+      locationAddress: "Avenida Recife, 386, 56800-000",
+      locationGPS: {
+        latitude: "-8.034432794113949",
+        longitude: "-34.87095323678137",
+        label: "Classic Hall",
+      },
+      city: getCities()[0],
+      dateInfo: {
+        startDate: 1700514000694,
+        endDate: 1700521200694,
+      },
+      price: "R$ 10,00",
+    },
+    {
+      idEvent: 2,
+      eventName: "The Wombats",
+      locationName: "Sala Apolo",
+      locationAddress: "Avenida Recife, 386, 56800-000",
+      locationGPS: {
+        latitude: "-8.034432794113949",
+        longitude: "-34.87095323678137",
+        label: "Classic Hall",
+      },
+      city: getCities()[0],
+      dateInfo: {
+        startDate: 1700514000694,
+        endDate: 1700521200694,
+      },
+      price: "R$ 20,00",
+    },
+    {
+      idEvent: 3,
+      eventName: "Foster The People",
+      locationName: "La Monumental",
+      locationAddress: "Avenida Recife, 386, 56800-000",
+      locationGPS: {
+        latitude: "-8.034432794113949",
+        longitude: "-34.87095323678137",
+        label: "Classic Hall",
+      },
+      city: getCities()[0],
+      dateInfo: {
+        startDate: 1700514000694,
+        endDate: 1700521200694,
+      },
+      price: "R$ 30,00",
+    },
+    {
+      idEvent: 4,
+      eventName: "La Rosalia",
+      locationName: "Maracanã",
+      locationAddress: "Maracanã, Rio de Janeiro",
+      locationGPS: {
+        latitude: "-22.912324276023295",
+        longitude: "-43.226815212389724",
+        label: "Maracanã",
+      },
+      city: getCities()[0],
+      dateInfo: {
+        startDate: 1700514000694,
+        endDate: 1700521200694,
+      },
+      highlight: true,
+      price: "R$ 50,00",
+    },
+    {
+      idEvent: 5,
+      eventName: "La Rosalia",
+      locationName: "Maracanã",
+      locationAddress: "Maracanã, Rio de Janeiro",
+      locationGPS: {
+        latitude: "-22.912324276023295",
+        longitude: "-43.226815212389724",
+        label: "Maracanã",
+      },
+      city: getCities()[1],
+      dateInfo: {
+        startDate: 1700514000694,
+        endDate: 1700521200694,
+      },
+      price: "R$ 50,00",
+    },
+    {
+      idEvent: 6,
+      eventName: "The Kooks",
+      locationName: "Razzmatazz",
+      locationAddress: "Avenida Recife, 386, 56800-000",
+      locationGPS: {
+        latitude: "-8.034432794113949",
+        longitude: "-34.87095323678137",
+        label: "Classic Hall",
+      },
+      city: getCities()[1],
+      dateInfo: {
+        startDate: 1700514000694,
+        endDate: 1700521200694,
+      },
+      price: "R$ 10,00",
+      highlight: true,
+    },
+    {
+      idEvent: 7,
+      eventName: "The Wombats",
+      locationName: "Sala Apolo",
+      locationAddress: "Avenida Recife, 386, 56800-000",
+      locationGPS: {
+        latitude: "-8.034432794113949",
+        longitude: "-34.87095323678137",
+        label: "Classic Hall",
+      },
+      city: getCities()[2],
+      dateInfo: {
+        startDate: 1700514000694,
+        endDate: 1700521200694,
+      },
+      price: "R$ 20,00",
+    },
+    {
+      idEvent: 8,
+      eventName: "Foster The People",
+      locationName: "La Monumental",
+      locationAddress: "Avenida Recife, 386, 56800-000",
+      locationGPS: {
+        latitude: "-8.034432794113949",
+        longitude: "-34.87095323678137",
+        label: "Classic Hall",
+      },
+      city: getCities()[2],
+      dateInfo: {
+        startDate: 1700514000694,
+        endDate: 1700521200694,
+      },
+      price: "R$ 30,00",
+    },
+    {
+      idEvent: 9,
+      eventName: "La Rosalia",
+      locationName: "Maracanã",
+      locationAddress: "Maracanã, Rio de Janeiro",
+      locationGPS: {
+        latitude: "-22.912324276023295",
+        longitude: "-43.226815212389724",
+        label: "Maracanã",
+      },
+      city: getCities()[1],
+      dateInfo: {
+        startDate: 1700514000694,
+        endDate: 1700521200694,
+      },
+      price: "R$ 50,00",
+    },
+  ];
+
+  if (idCity === undefined || idCity === null || idCity === -1) {
+    return eventsDB;
+  }
+
+  const eventsFiltered = eventsDB.filter((item) => item.city.id === idCity);
+  return eventsFiltered;
+};
+
+export const getEvent = (idEvent: number) => {
+  return getEvents(-1).find((item) => item.idEvent === idEvent);
+};
 
 export const getImage = (idEvent: number) => {
-  switch (idEvent) {
-    case 1:
-      return require("./banda2.png");
-    case 2:
-      return require("./banda3.png");
-    case 3:
-      return require("./banda4.png");
-    case 4:
-      return require("./banda1.png");
+  if ([1,6].includes(idEvent)) {
+    return require("./banda2.png");
+  }
+  if ([2,7].includes(idEvent)) {
+    return require("./banda3.png");
+  }
+  if ([3,8].includes(idEvent)) {
+    return require("./banda4.png");
+  }
+  if ([4,5].includes(idEvent)) {
+    return require("./banda1.png");
+  }
+  if ([9].includes(idEvent)) {
+    return require("./banda1grey.png");
   }
 };
+
+
+export const getTickets = (): TicketType[] => {
+  return [{
+    id: 1,
+    amount: 1,
+    event: getEvent(1),
+    past: false
+  },
+  {
+    id: 2,
+    amount: 2,
+    event: getEvent(2),
+    past: false
+  },
+  {
+    id: 3,
+    amount: 2,
+    event: getEvent(9),
+    past: true
+  }
+]
+}
+
--- src/common/types.ts
+++ src/common/types.ts
@@ -1,24 +1,37 @@
 export type UserType = {
-    id: number
-    email: string
-    username: string
-  };
+  id: number;
+  email: string;
+  username: string;
+};
 
-  
+export type CityType = {
+  id: number;
+  state: string;
+  city: string;
+};
 export interface EventCardType {
-  idEvent: number
-  locationAddress: string
+  idEvent: number;
+  locationAddress: string;
   locationGPS: {
-    latitude: string
-    longitude: string
-    label: string
-  }
+    latitude: string;
+    longitude: string;
+    label: string;
+  };
+  city : CityType;
   dateInfo: {
-    startDate: number
-    endDate: number
-  }
-  eventName: string
-  locationName: string
-  highlight?: boolean
-  price: string
+    startDate: number;
+    endDate: number;
+  };
+  eventName: string;
+  locationName: string;
+  highlight?: boolean;
+  price: string;
 }
+
+
+export type TicketType = {
+  id: number;
+  event: EventCardType;
+  amount: number
+  past: boolean
+};
