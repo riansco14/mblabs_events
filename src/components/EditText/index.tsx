a875923b42b5678b788bad26a30ac0615676fb76
Rian <rian.azevedo@qintess.com>
2023-08-20 22:48:29 -0300

    change: -add error field to validate

--- src/components/EditText/index.tsx
+++ src/components/EditText/index.tsx
@@ -1,11 +1,29 @@
-import React from "react"
-import { Container } from "./styles"
-import {TextInputProps, TextInput} from "react-native"
-import { useTheme } from "styled-components"
+import React from "react";
+import { Container } from "./styles";
+import { TextInputProps, TextInput } from "react-native";
+import { useTheme } from "styled-components";
+import { Text } from "../Text";
 
-export const EditText = React.forwardRef((props: TextInputProps, ref: React.Ref<TextInput>) =>{
-    const theme = useTheme()
-    return(<Container ref={ref} placeholderTextColor={theme.colors.placeholder_grey} {...props}>
+interface EditTextProps extends TextInputProps{
+    error?: string
+}
 
-    </Container>)
-})
+export const EditText = React.forwardRef(
+  ({error, ...rest}: EditTextProps, ref: React.Ref<TextInput>) => {
+    const theme = useTheme();
+    return (
+      <>
+        <Container
+          ref={ref}
+          placeholderTextColor={theme.colors.placeholder_grey}
+          {...rest}
+        ></Container>
+        {error ? (
+          <Text type="small" color={theme.colors.red} style={{marginTop: 6, fontWeight: "bold"}}>
+            {error}
+          </Text>
+        ) : null}
+      </>
+    );
+  }
+);
