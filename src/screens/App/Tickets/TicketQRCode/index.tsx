711c935f30ab2bb8abeb5a74046b741ea8c625de
Rian <rian.azevedo@qintess.com>
2023-08-21 23:23:59 -0300

    create: -screen to show tickets qr code

---
+++ src/screens/App/Tickets/TicketQRCode/index.tsx
@@ -0,0 +1,188 @@
+import React, { useRef } from "react";
+import {
+  Container,
+  Content,
+  Section,
+  SectionInfo,
+  TicketQRCodeContainer,
+  TicketQRCodeImage,
+} from "./styles";
+
+import { ScrollView, Animated } from "react-native";
+
+import DynamicHeader from "../../../../components/DynamicHeader";
+import { Text, Text as TextCustom } from "../../../../components/Text";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { Heading } from "../../../../components/Heading";
+import { Icon } from "../../../../components/Icon";
+import { useTheme } from "styled-components";
+import { useAppDispatch, useAppSelector } from "../../../../store/hook";
+import { addOrRemoveEventFavorite } from "../../../../store/event/eventSlice";
+import { useNavigation } from "@react-navigation/native";
+import { AuthStackParam } from "../../../../config/navigation/routes";
+import { getTicket } from "../../../../../database/db";
+import {
+  createEventInCalendar,
+  openMaps,
+  shareEvent,
+} from "../../../../services/system/System";
+
+import moment from "moment";
+
+export function TicketQRCode({ route }) {
+  const theme = useTheme();
+  const navigation = useNavigation<AuthStackParam>();
+
+  let scrollOffsetY = useRef(new Animated.Value(0)).current;
+  const { idTicket } = route.params;
+  const ticketData = getTicket(idTicket);
+
+  const eventData = ticketData.event;
+  const { idEvent } = eventData;
+
+  const eventsFavorites = useAppSelector(
+    (state) => state.event.eventsFavorites
+  );
+  const dispatch = useAppDispatch();
+
+  function handleAddOrRemoveFavorite(idEvent: number) {
+    dispatch(addOrRemoveEventFavorite({ idEvent }));
+  }
+
+  function handleGoBack() {
+    if (navigation.canGoBack()) {
+      navigation.goBack();
+    }
+  }
+
+  return (
+    <Container>
+      <DynamicHeader
+        idEvent={idEvent}
+        animHeaderValue={scrollOffsetY}
+        onPressGoBack={handleGoBack}
+        isLiked={eventsFavorites.includes(idEvent)}
+        onPressLikeButton={() => handleAddOrRemoveFavorite(idEvent)}
+        onPressShareButton={() => shareEvent(eventData)}
+      />
+      <ScrollView
+        scrollEventThrottle={8}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollOffsetY } } }],
+          { useNativeDriver: false }
+        )}
+        style={{ backgroundColor: "white" }}
+        contentContainerStyle={{ height: 800 }}
+      >
+        <Content>
+          <Heading type="h4">{eventData.eventName}</Heading>
+          <Section>
+            <Icon
+              name="calendar"
+              color={theme.colors.black}
+              width={18}
+              height={18}
+            />
+            <SectionInfo>
+              <TextCustom
+                type="defaultBold"
+                color={theme.colors.font_dark}
+                style={{ lineHeight: 24 }}
+              >
+                {moment(eventData.dateInfo.startDate)
+                  .format("ddd, MMM D · kk:mm")
+                  .toUpperCase()}
+              </TextCustom>
+              <TextCustom
+                type="small"
+                color={theme.colors.font_dark}
+                style={{ lineHeight: 20 }}
+              >
+                {moment(eventData.dateInfo.startDate)
+                  .format("kk:mm")
+                  .toUpperCase()}
+                {"\t-\t"}
+                {moment(eventData.dateInfo.endDate)
+                  .format("kk:mm")
+                  .toUpperCase()}
+              </TextCustom>
+              <TouchableOpacity
+                onPress={() => {
+                  createEventInCalendar({
+                    title: eventData.eventName,
+                    startDate: moment(eventData.dateInfo.startDate).format(
+                      "YYYY-MM-DDTHH:mm:ss.SSS[Z]"
+                    ),
+                    endDate: moment(eventData.dateInfo.endDate).format(
+                      "YYYY-MM-DDTHH:mm:ss.SSS[Z]"
+                    ),
+                  });
+                }}
+              >
+                <TextCustom
+                  type="small"
+                  color={theme.colors.blue}
+                  style={{ fontWeight: "bold", lineHeight: 14 }}
+                >
+                  Adicionar ao calendário
+                </TextCustom>
+              </TouchableOpacity>
+            </SectionInfo>
+          </Section>
+
+          <Section>
+            <Icon
+              name="map_pin"
+              color={theme.colors.black}
+              width={18}
+              height={18}
+            />
+            <SectionInfo>
+              <TextCustom
+                type="defaultBold"
+                color={theme.colors.font_dark}
+                style={{ lineHeight: 24 }}
+              >
+                {eventData.locationName}
+              </TextCustom>
+              <TextCustom
+                type="small"
+                color={theme.colors.font_dark}
+                style={{ lineHeight: 20 }}
+              >
+                {eventData.locationAddress}
+              </TextCustom>
+              <TouchableOpacity onPress={() => openMaps(eventData.locationGPS)}>
+                <TextCustom
+                  type="small"
+                  color={theme.colors.blue}
+                  style={{ fontWeight: "bold", lineHeight: 14 }}
+                >
+                  Ver no mapa
+                </TextCustom>
+              </TouchableOpacity>
+            </SectionInfo>
+          </Section>
+
+          <Heading type="h4" style={{ marginTop: 16 }}>
+            Ingressos
+          </Heading>
+          {new Array(ticketData.amount).fill(0).map((item, index) => {
+            return (
+              <TicketQRCodeContainer
+                style={{ marginTop: index === 0 ? 0 : 32 }}
+              >
+                <Text type="defaultBold" color={theme.colors.font_dark}>
+                  Ingresso {index + 1}
+                </Text>
+                <TicketQRCodeImage
+                  source={require("../../../../assets/ilustrations/qrCode.png")}
+                />
+              </TicketQRCodeContainer>
+            );
+          })}
+        </Content>
+      </ScrollView>
+    </Container>
+  );
+}
---
+++ src/screens/App/Tickets/TicketQRCode/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  margin: 0px;
+`;
+
+export const Content = styled.View`
+  padding: 0px 16px;
+  padding-top: 20px;
+`;
+
+export const Section = styled.View`
+  flex-direction: row;
+  margin-top: 24px;
+`;
+
+export const SectionInfo = styled.View`
+    margin-left: 12px;
+`;
+
+
+export const SectionAbout = styled.View`
+  margin-top: 36px;
+`;
+
+export const Footer = styled.View`
+  flex-direction: row;
+  height: 100px;
+  padding: 0px 16px;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.grey};
+`;
+
+export const FooterSection = styled.View`
+    flex: 0.5;
+`;
+
+export const TicketQRCodeContainer = styled.View`
+  align-items: center;
+`
+
+export const TicketQRCodeImage = styled.Image`
+  margin-top: 16px;
+  width: 200px;
+  height: 200px;
+`
